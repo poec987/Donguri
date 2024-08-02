ffc83a6d89f2d2a05645e27e8ff3c016da4f742f
poec987 <poedev08@outlook.com>
2024-08-01 21:26:46 -0400

    Fixing some errors

--- src/tilegodhax.cpp
+++ src/tilegodhax.cpp
@@ -8,12 +8,12 @@ void copyTilesInLocation(Level::Area::Location *loc, f32 x, f32 y, u8 layer, boo
 	
 	for (int row = 0; row < rows; row++) {
 		for (int column = 0; column < cols; column++) {
-			u16 *dst = TileMgr::instance->getTilePtr(x + row * 16, -y + column * 16, layer, 0);
+			u16 *dst = TileMgr::instance()->getTilePtrCurrentArea(x + row * 16, -y + column * 16, layer, 0);
 			if (destroy) {
 				*dst = 0xFFFF;
 			}
 			else {
-				u16 *src = TileMgr::instance->getTilePtr(loc->x + row * 16, loc->y + column * 16, 0, 0);
+				u16 *src = TileMgr::instance()->getTilePtrCurrentArea(loc->x + row * 16, loc->y + column * 16, 0, 0);
 				*dst = *src;
 			}
 		}
@@ -21,14 +21,12 @@ void copyTilesInLocation(Level::Area::Location *loc, f32 x, f32 y, u8 layer, boo
 }
 
 extern "C" void copyTilesFromLocation(u8 locationId, u8 layer, bool spawns, f32 x, f32 y) {
-	u8 areaNum = LevelInfo::instance->area;
-	Level::Area *area = Level::instance->getArea(areaNum);
+	u8 areaNum = LevelInfo::instance()->area;
+	Level::Area *area = Level::instance()->getArea(areaNum);
 	Level::Area::Location *loc = area->getLocation(0, locationId);
 	
 	if (loc) {
 		copyTilesInLocation(loc, x, y, layer, !spawns);
-	} else {
-		DEBUG("Tsuru tile god location not found.");
 	}
 }
 
@@ -49,7 +47,7 @@ tileGodHacks:
 	lfs f1, 0x6C(r26)		// X
 	lfs f2, 0x70(r26)		// Y
 	bl copyTilesFromLocation
-	b 0xF2E5DB0
+	b 0x1725DB0
 
 .global tileGodHacks_retail
 tileGodHacks_retail:
